898b91f18dbcbaf58357dd9982743684f9f92e85
TrygveTafjord <122460925+TrygveTafjord@users.noreply.github.com>
2024-08-26 18:38:11 +0200

    made the get functions purer, due to id's changing per season <first_name  second_name> is binder for data. Ready to create the player_predict_points function

--- predict_player_points.py
+++ predict_player_points.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from utils.get_data import get_last_five_matches_player_data, get_upcoming_fixture_data, WAS_HOME, STRENGHT_DIFFERENCE, ATTACK_STRENGHT_DIFFERENCE, DEFENSE_STRENGHT_DIFFERENCE
+from utils.get_data import get_last_five_matches_player_data, get_upcoming_fixture_data, get_player_metadata, WAS_HOME, STRENGHT_DIFFERENCE, ATTACK_STRENGHT_DIFFERENCE, DEFENSE_STRENGHT_DIFFERENCE
 from utils.teams import team_name_to_id
 
 fwd_data_df = get_last_five_matches_player_data("FWD")
 mid_data_df = get_last_five_matches_player_data("MID")
 def_data_df = get_last_five_matches_player_data("DEF")
 gk_data_df = get_last_five_matches_player_data("GK")    
+player_metadata_df = get_player_metadata()
 
 player_prediction_df = pd.DataFrame
 
@@ -13,61 +14,15 @@ NUM_FIXTURES_AHEAD = 5
 
 upcoming_fixtures = get_upcoming_fixture_data(NUM_FIXTURES_AHEAD)
 
-for fwd in fwd_data_df.iterrows():
-        fixture_data = upcoming_fixtures[team_name_to_id[fwd['team_name']]]
-        player_prediction_df['name', 'id', 'team', 'position'] = fwd['name', 'id', 'team', 'FWD']
-        for fixture_nr in range(NUM_FIXTURES_AHEAD):
-            fwd['was_home'] = fixture_data[fixture_nr][WAS_HOME]
-            fwd['strength_difference'] = fixture_data[fixture_nr][STRENGHT_DIFFERENCE]
-            fwd['attack_strength_difference'] = fixture_data[fixture_nr][ATTACK_STRENGHT_DIFFERENCE]
-            #calculate predicted points for this fixture         
-
-        fwd['was_home'] = fixture_data[fixture_nr][WAS_HOME]
-        fwd['strength_difference'] = fixture_data[fixture_nr][STRENGHT_DIFFERENCE]
-        fwd['attack_strength_difference'] = fixture_data[fixture_nr][ATTACK_STRENGHT_DIFFERENCE]
-        #calculate predicted points for this fixture
-        #add to player_prediction_df 
 
 
-for fixture_nr in range(NUM_FIXTURES_AHEAD):
-    for fwd in fwd_data_df.iterrows():
-        fixture_data = upcoming_fixtures[team_name_to_id[fwd['team_name']]]
-        fwd['was_home'] = fixture_data[fixture_nr][WAS_HOME]
-        fwd['strength_difference'] = fixture_data[fixture_nr][STRENGHT_DIFFERENCE]
-        fwd['attack_strength_difference'] = fixture_data[fixture_nr][ATTACK_STRENGHT_DIFFERENCE]
-        #calculate predicted points for this fixture
-        #add to player_prediction_df 
+## iterate over players in player_metadata
+## by using position, for each player, get the upcoming fixtures by checking the club id in the upcoming_fixtures dictionary
+## by using position, get the correct lagged data dataframe for the player
+## by using position, use the correct model to calculate the predicted points for the player
+## add the metadata and predicted points to the player_prediction_df
+## voila
 
-    for mid in mid_data_df.iterrows():
-        fixture_data = upcoming_fixtures[team_name_to_id[mid['team_name']]]
-        mid['was_home'] = fixture_data[fixture_nr][WAS_HOME]
-        mid['strength_difference'] = fixture_data[fixture_nr][STRENGHT_DIFFERENCE]
-        mid['attack_strength_difference'] = fixture_data[fixture_nr][ATTACK_STRENGHT_DIFFERENCE]
-        #calculate predicted points for this fixture
-        #add to player_prediction_df¨
-
-    for defender in def_data_df.iterrows():
-        fixture_data = upcoming_fixtures[team_name_to_id[defender['team_name']]]
-        defender['was_home'] = fixture_data[fixture_nr][WAS_HOME]
-        defender['strength_difference'] = fixture_data[fixture_nr][STRENGHT_DIFFERENCE]
-        defender['defense_strength_difference'] = fixture_data[fixture_nr][ATTACK_STRENGHT_DIFFERENCE]
-        #calculate predicted points for this fixture
-        #add to player_prediction_df
-
-    for gk in gk_data_df.iterrows():
-        fixture_data = upcoming_fixtures[team_name_to_id[gk['team_name']]]
-        gk['was_home'] = fixture_data[fixture_nr][WAS_HOME]
-        gk['strength_difference'] = fixture_data[fixture_nr][STRENGHT_DIFFERENCE]
-        gk['defense_strength_difference'] = fixture_data[fixture_nr][ATTACK_STRENGHT_DIFFERENCE]
-        #calculate predicted points for this fixture
-        #add to player_prediction_df
-
-    
-
-
-
-
-#for i in range(NUM_FIXTURES_AHEAD):
     
 
 
