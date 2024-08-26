2e39dc666b788a84531a620b795ccbb6f644643e
TrygveTafjord <122460925+TrygveTafjord@users.noreply.github.com>
2024-08-26 18:38:11 +0200

    made the get functions purer, due to id's changing per season <first_name  second_name> is binder for data. Ready to create the player_predict_points function

--- utils/feature_selection_config.py
+++ utils/feature_selection_config.py
@@ -4,77 +4,40 @@ The results are stored here for the final model training. """
 
 fpl_features_by_position = {
     "GK": [
-                'name', 'position', 'team', 'element', 'xP', 'bonus', 'bps', 'clean_sheets',  
+                'name', 'position', 'GW', 'xP', 'bonus', 'bps', 'clean_sheets',  
                 'expected_goals_conceded', 'ict_index', 'influence', 'minutes', 
                 'opponent_team', 'own_goals', 'red_cards', 'saves',
                 'selected', 'team_a_score', 'team_h_score', 'total_points', 
                 'transfers_balance', 'transfers_in', 'transfers_out', 'value', 'was_home', 
-                'yellow_cards', 'GW'
+                'yellow_cards', 
                 ],
     "DEF": [
-                'name', 'position', 'team', 'element', 'xP', 'assists', 'bonus', 'bps', 'clean_sheets', 
+                'name', 'position', 'GW', 'xP', 'assists', 'bonus', 'bps', 'clean_sheets', 
                 'creativity', 'expected_assists', 'expected_goal_involvements', 
                 'expected_goals','expected_goals_conceded', 'goals_scored', 'ict_index', 'influence', 'minutes', 
                 'opponent_team', 'own_goals', 'penalties_missed', 'red_cards', 
                 'selected', 'team_a_score', 'team_h_score', 'threat', 'total_points', 
                 'transfers_balance', 'transfers_in', 'transfers_out', 'value', 'was_home', 
-                'yellow_cards', 'GW'
+                'yellow_cards'
                 ],
     "MID": [
-                'name', 'position', 'team', 'element', 'xP', 'assists', 'bonus', 'bps', 'clean_sheets', 
+                'name', 'position', 'GW', 'xP', 'assists', 'bonus', 'bps', 'clean_sheets', 
                 'creativity', 'expected_assists', 'expected_goal_involvements', 
                 'expected_goals','expected_goals_conceded', 'goals_scored', 'ict_index', 'influence', 'minutes', 
                 'opponent_team', 'own_goals', 'penalties_missed', 'red_cards', 
                 'selected', 'team_a_score', 'team_h_score', 'threat', 'total_points', 
                 'transfers_balance', 'transfers_in', 'transfers_out', 'value', 'was_home', 
-                'yellow_cards', 'GW'
+                'yellow_cards'
                 ],
     "FWD": [
-                'name', 'position', 'team', 'element', 'xP', 'assists', 'bonus', 'bps', 
+                'name', 'position', 'GW', 'xP', 'assists', 'bonus', 'bps', 
                 'creativity', 'expected_assists', 'expected_goal_involvements', 
                 'expected_goals', 'goals_scored', 'ict_index', 'influence', 'minutes', 
                 'opponent_team', 'own_goals', 'penalties_missed', 'red_cards', 
                 'selected', 'team_a_score', 'team_h_score', 'threat', 'total_points', 
                 'transfers_balance', 'transfers_in', 'transfers_out', 'value', 'was_home', 
-                'yellow_cards', 'GW'
+                'yellow_cards'
                 ]
 }
 
-fpl_lagged_features_by_position = {
-    "GK" : [
-                'xP', 'bonus', 'bps', 'clean_sheets', 
-                'expected_goals_conceded', 'ict_index', 'influence', 
-                'minutes', 'own_goals', 'red_cards', 'saves',
-                'selected', 'team_a_score', 'team_h_score', 
-                'total_points', 'transfers_balance', 'transfers_in', 
-                'transfers_out', 'value', 'was_home', 'yellow_cards'
-    ],
-    "DEF" : [
-                'xP', 'assists', 'bonus', 'bps', 'clean_sheets','creativity', 
-                'expected_assists', 'expected_goal_involvements', 
-                'expected_goals', 'expected_goals_conceded', 'goals_scored', 'ict_index', 'influence', 
-                'minutes', 'own_goals', 'penalties_missed', 'red_cards', 
-                'selected', 'team_a_score', 'team_h_score', 'threat', 
-                'total_points', 'transfers_balance', 'transfers_in', 
-                'transfers_out', 'value', 'was_home', 'yellow_cards'
-    ],
-    "MID" : [
-                'xP', 'assists', 'bonus', 'bps', 'clean_sheets','creativity', 
-                'expected_assists', 'expected_goal_involvements', 
-                'expected_goals', 'expected_goals_conceded', 'goals_scored', 'ict_index', 'influence', 
-                'minutes', 'own_goals', 'penalties_missed', 'red_cards', 
-                'selected', 'team_a_score', 'team_h_score', 'threat', 
-                'total_points', 'transfers_balance', 'transfers_in', 
-                'transfers_out', 'value', 'was_home', 'yellow_cards'
-    ],
-    "FWD" : [
-                'xP', 'assists', 'bonus', 'bps', 'creativity', 
-                'expected_assists', 'expected_goal_involvements', 
-                'expected_goals', 'goals_scored', 'ict_index', 'influence', 
-                'minutes', 'own_goals', 'penalties_missed', 'red_cards', 
-                'selected', 'team_a_score', 'team_h_score', 'threat', 
-                'total_points', 'transfers_balance', 'transfers_in', 
-                'transfers_out', 'value', 
-    ]
-}
 
